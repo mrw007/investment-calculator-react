dd861b82fdbe30543391677ae6f83334a0206a45
mrw007 <mr.wahib@gmail.com>
2026-05-18 10:36:38 +0100

    Implement results table to display investment calculations

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,7 +1,43 @@
-import { calculateInvestmentResults } from "../util/investment";
+import { calculateInvestmentResults, formatter } from "../util/investment";
 
 export default function Results({ inputs }) {
   const resultsData = calculateInvestmentResults(inputs);
+  const initialInvestment =
+    resultsData[0].valueEndOfYear -
+    resultsData[0].interest -
+    resultsData[0].annualInvestment;
 
-  return <p>Results will be displayed here</p>;
+  return (
+    <table id="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Investment</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <tbody>
+        {resultsData.map((yearData) => {
+          const totalInterset =
+            yearData.valueEndOfYear -
+            yearData.annualInvestment * yearData.year -
+            initialInvestment;
+
+          const investedCapital = yearData.valueEndOfYear - totalInterset;
+
+          return (
+            <tr key={yearData.year}>
+              <td>{yearData.year}</td>
+              <td>{formatter.format(yearData.valueEndOfYear)}</td>
+              <td>{formatter.format(yearData.interest)}</td>
+              <td>{formatter.format(totalInterset)}</td>
+              <td>{formatter.format(investedCapital)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 }
